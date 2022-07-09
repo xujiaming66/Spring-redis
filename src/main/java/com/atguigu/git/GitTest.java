33988153bb5d422c061955239c882f9f430dac53
xujiaming <xujiaming@atguigu.com>
2022-07-10 00:43:24 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hot fix test");
+
     }
 }
